f9a9503918d0df9e946d2499a599c36deab99084
Thor Muto Asmund <thorasmund@gmail.com>
2018-09-24 13:36:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -41,29 +41,37 @@ $( document ).ready(function() {
     }
   }
 
-  function check(b) {
+  function checkTurnWon(b, turn) {
     const rows = [
       [0,1,2], [3,4,5], [6,7,8],
       [0,3,6],[1,4,7],[2,5,8],
       [0,4,8], [2,4,6]
     ];
-    for (var i=1; i<=2; ++i) {
-      for (var r in rows) {
-        var row = rows[r];
-        if (board[row[0]] == i && board[row[1]] == i && board[row[2]] == i) {
-          return i;
-        }
+    for (var r in rows) {
+      var row = rows[r];
+      if (b[row[0]] == turn && b[row[1]] == turn && b[row[2]] == turn) {
+        return true;
       }
     }
-    return 0;
+    return false;
   }
 
   function checkWin() {
-    var i = check(board);
-    if (i > 0) {
+    var winner = -1
+    if (checkTurnWon(board, 1)) {
+      winner = 1;
+    }
+    else if (checkTurnWon(board, 2)) {
+      winner = 2;  
+    }
+    else if (board.every(c => c > 0)) {
+      winner = 0;
+    }
+    
+    if (winner > -1) {
       gameStarted = false;
-      winner = i;
-      var text = winner == 1 && computerStarts || winner == 2 && !computerStarts ? 'YOU LOST' : 'YOU WON';
+      var text = winner == 0 ? 'DRAW' : ( 
+        winner == 1 && computerStarts || winner == 2 && !computerStarts ? 'YOU LOST' : 'YOU WON');
       console.log(text)
       $('#resultMessage').text(text);
     }
@@ -73,59 +81,88 @@ $( document ).ready(function() {
     thinking = true;
 
     var cpu = computerStarts ? 1 : 2;
-    var cnt = 0;
-    var ev = function(b, turn) {
-      var possibleMoves = [];
+
+    function inv(turn) {
+      return turn == 1 ? 2 : 1;
+    }
+
+    var evalBoardSelf = function(b, turn) {
+      // Check if the last (opponent) move returned in a win
+      var v = checkTurnWon(b, inv(turn));
+      if (v) {
+        return 1000;
+      }
+      var moves = [];
+      
       for (var i=0; i<9; ++i) {
         if (b[i] == 0) {
-          cnt++;
-          if (cnt % 10000 == 0) {
-            console.log(cnt);
-          }
           var b2 = b.slice();
           b2[i] = turn;
-          var v = check(board, cpu);
-
-          if (v != 0) {
-            if (turn != cpu) {
-              return -1;
-            }
-            else {
-              return 1;
-            }
-          }
-
-          var w = ev(b2, turn == 1 ? 2 : 1);
-          if (turn == cpu) {
-            if (w == -1) {
-              // bad move
-            }
-            else if (w == 1) {
-              possibleMoves.unshift(i);              
-            }
-            else {
-              possibleMoves.push(i);
-            }
-          }
-          else {
-            
-          } 
+          var e = evalBoardOpponent(b2, inv(turn))
+          moves.push({pos: i, ev: e});
         }
       }
-      return 0;
-      
-    }
-    console.log('tried', cnt);
-    
-    ev(board.slice(), cpu);
 
-    for (var i=0; i<9; ++i) {
-      if (board[i] == 0) {
-        board[i] = computerStarts ? 1 : 2;
-        $('#f'+i).text(board[i] == 1 ? 'O' : 'X');
-        break;
+      if (moves.length == 0) {
+        return 0;
       }
+
+      var val = moves.reduce((a,c) => a + c.ev, 0) / moves.length;
+
+      return -val;
+
+      // var isNotWin = moves.length == 0 || moves.every(c => c.ev < 1);
+
+      // return isNotWin ? 0 : -1;
     }
+
+    var evalBoardOpponent = function(b, turn) {
+      // Check if the last (self) move returned in a win
+      var v = checkTurnWon(b, inv(turn));
+      if (v) {
+        return 1000;
+      }
+      var moves = [];
+      
+      for (var i=0; i<9; ++i) {
+        if (b[i] == 0) {
+          var b2 = b.slice();
+          b2[i] = turn;
+          var e = evalBoardSelf(b2, inv(turn))
+          moves.push({pos: i, ev: e});
+        }
+      }
+
+      if (moves.length == 0) {
+        return 0;
+      }
+
+      var val = moves.reduce((a,c) => a + c.ev, 0) / moves.length;
+
+      return -val;
+      
+      // var isNotWin = moves.length == 0 || moves.every(c => c.ev < 1);
+
+      // return isNotWin ? 0 : -1;
+    }
+
+    var getMove = function(b, turn) {
+      var moves = [];
+      for (var i=0; i<9; ++i) {
+        if (b[i] == 0) {
+          var b2 = b.slice();
+          b2[i] = turn;
+          var e = evalBoardOpponent(b2, inv(turn))
+          moves.push({pos: i, ev: e});
+        }
+      }
+
+      return moves.reduce((a,c) => c.ev > a.ev ? c : a, {pos:0, ev:-1000});
+    }
+
+    var move = getMove(board.slice(), cpu);
+    board[move.pos] = cpu;
+    $('#f' + move.pos).text(board[move.pos] == 1 ? 'O' : 'X');
 
     thinking = false;
     checkWin();
